aabe54e301c86010b6c9c9bff4ededc6c4c314ef
chames06 <oukelul@gmail.com>
2025-11-18 06:40:22 +0100

    2eme

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,10 +1,10 @@
-#ifndef MAIN_H
-# define MAIN_H
+/* 0-reset_to_98.c */
+#include "main.h"
+#include <stddef.h>   /* <-- Ajouté */
 
-/* Prototypes */
-void reset_to_98(int *n);
-
-/* Prototype de _putchar (déjà fourni par l’environnement) */
-int _putchar(char c);
-
-#endif /* MAIN_H */
+void reset_to_98(int *n)
+{
+    if (n == NULL)   /* protection contre un pointeur nul */
+        return;
+    *n = 98;         /* mise à jour de la valeur pointée */
+}
