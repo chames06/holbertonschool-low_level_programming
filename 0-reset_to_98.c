25d389f18f50a2e42997bf259add2e90bf8a64cc
chames06 <oukelul@gmail.com>
2025-11-18 06:33:30 +0100

    1er

---
+++ 0-reset_to_98.c
@@ -0,0 +1,13 @@
+/* 0-reset_to_98.c */
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value pointed to by n to 98
+ * @n: Pointer to an integer
+ *
+ * Description: Updates the value of the integer pointed to by n to 98
+ */
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
