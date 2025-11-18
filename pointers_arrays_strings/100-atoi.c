3e5e69efe1c3083edcfe540ffae174c286d2c268
chames06 <oukelul@gmail.com>
2025-11-18 07:04:27 +0100

    6eme

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <limits.h>
 
 /**
- * _atoi - converts a string to an integer
+ * _atoi - converts a string to an integer with overflow protection
  * @s: pointer to the string
  *
  * Return: converted integer
@@ -22,8 +23,22 @@ int _atoi(char *s)
 		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			int digit = s[i] - '0';
+
+			/* Vérification pré-overflow */
+			if (sign == 1)
+			{
+				if (num > (INT_MAX - digit) / 10)
+					return (INT_MAX);
+			}
+			else
+			{
+				if (num > (-(INT_MIN + digit)) / 10)
+					return (INT_MIN);
+			}
+
+			num = num * 10 + digit;
 			found = 1;
-			num = num * 10 + (s[i] - '0');
 		}
 		else if (found)
 			break;
@@ -31,5 +46,5 @@ int _atoi(char *s)
 		i++;
 	}
 
-	return (sign * num);
+	return (num * sign);
 }
